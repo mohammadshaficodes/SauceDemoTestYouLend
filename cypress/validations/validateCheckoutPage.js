be1f591f9a15f0c11b041d7f058f547d8f95352b
Mohammad Shafi <ibrahim-shafi@hotmail.co.uk>
2022-06-11 15:00:15 +0100

    Adding valitadtion of total price in checkout

--- cypress/validations/validateCheckoutPage.js
+++ cypress/validations/validateCheckoutPage.js
@@ -11,98 +11,50 @@ export const validateFinishButton = () => {
 };
 
 export const validateTotalPrice = () => {
-
-    // getting the individual price of the fleece
-    CHECKOUT_PAGE.fleecePrice().invoke('text').then((text) => {
-        cy.log(text);
-    });
-
-    // getting the individual price of the t shirt
-    CHECKOUT_PAGE.boltTShirtPrice().invoke('text').then((text) => {
-        cy.log(text);
-    });
-
     // getting the subtotal of both items 
     CHECKOUT_PAGE.itemPriceSubtotal().invoke('text').then((text) => {
         //get the number subtotal
-        cy.log(text);
         let strippedText = text.split(' ');
-        cy.log(strippedText);
         let subtotal = strippedText[2].split('$');
-        let numSubtotal = subtotal[1];
-        cy.log(numSubtotal);
+        let numSubtotal = parseFloat(subtotal[1]);
+
+        // getting the individual price of the fleece
+        CHECKOUT_PAGE.fleecePrice().invoke('text').then((text) => {
+            let fleeceCostRaw = text;
+            let strippedFleeceCost = fleeceCostRaw.split('$');
+            let fleeceCost = parseFloat(strippedFleeceCost[1]);
+
+            // getting the individual price of the t shirt
+            CHECKOUT_PAGE.boltTShirtPrice().invoke('text').then((text) => {
+                let boltTShirtCostRaw = text;
+                let strippedTShirtCost = boltTShirtCostRaw.split('$');
+                let boltTShirtCost = parseFloat(strippedTShirtCost[1]);
+
+                let sumOfItems = parseFloat(boltTShirtCost) + parseFloat(fleeceCost);
+                let calculatedTax = (0.08*sumOfItems).toFixed(2);
+                let totalIncTax = parseFloat(sumOfItems) + parseFloat(calculatedTax);
+
+                // getting the tax amount
+                CHECKOUT_PAGE.itemPriceTax().invoke('text').then((text) => {
+                    //get the Tax
+                    let strippedText = text.split(' ');
+                    let taxTotal = strippedText[1].split('$');
+                    let tax = taxTotal[1];
+
+                    // getting the total including tax 
+                    CHECKOUT_PAGE.itemPriceTotal().invoke('text').then((text) => {
+                        //get the Tax
+                        let strippedText = text.split(' ');
+                        let numTotal = strippedText[1].split('$');
+                        let total = parseFloat(numTotal[1]);
+
+                        // Assertions
+                        expect(sumOfItems).to.equal(numSubtotal);
+                        expect(calculatedTax).to.equal(tax);
+                        expect(totalIncTax).to.equal(total);
+                    });
+                });
+            });
+        });
     });
-
-    // getting the tax amount
-    CHECKOUT_PAGE.itemPriceTax().invoke('text').then((text) => {
-        //get the Tax
-        cy.log(text);
-        let strippedText = text.split(' ');
-        cy.log(strippedText);
-        let taxTotal = strippedText[1].split('$');
-        let tax = taxTotal[1];
-        cy.log(tax);
-    });
-
-    // getting the total including tax 
-    CHECKOUT_PAGE.itemPriceTotal().invoke('text').then((text) => {
-        //get the Tax
-        cy.log(text);
-        let strippedText = text.split(' ');
-        cy.log(strippedText);
-        let numTotal = strippedText[1].split('$');
-        let total = numTotal[1];
-        cy.log(total);
-    });
-
-
-    // let theItemPrice = CHECKOUT_PAGE.itemPriceSubtotal();
-    // theItemPrice.invoke('text').then((text) => {
-    //     let theItemPrice1 = text;
-
-        // CHECKOUT_PAGE.itemPrices().invoke('text').then((text) => {
-        //     let yieldedText = text;
-        //     let replacedText = yieldedText.split("$");
-        //     let shiftedText = replacedText.shift();
-        //     const totalPrice = parseFloat(replacedText[0]) + parseFloat(replacedText[1]);
-        //     const tax = (0.08*totalPrice).toFixed(2);
-        //     const realTotal = (parseFloat(totalPrice) + parseFloat(tax)).toFixed(2);
-    
-        //     cy.log(realTotal);
-    
-        //     return realTotal;
-        // });
-
-
-
-    // }).should('eq', '71.26');
-
-    // CHECKOUT_PAGE.itemPrices().invoke('text').then((text) => {
-    //     let yieldedText = text;
-    //     let replacedText = yieldedText.split("$");
-    //     let shiftedText = replacedText.shift();
-    //     const totalPrice = parseFloat(replacedText[0]) + parseFloat(replacedText[1]);
-    //     const tax = (0.08*totalPrice).toFixed(2);
-    //     const realTotal = (parseFloat(totalPrice) + parseFloat(tax)).toFixed(2);
-
-    //     cy.log(realTotal);
-
-    //     return realTotal;
-    // }).should('eq', theItemPrice);
-    // //.should('eq', '71.26')
-
-        // CHECKOUT_PAGE.itemPrices().invoke('text').then((text) => {
-    //     let yieldedText = text;
-    //     let replacedText = yieldedText.split("$");
-    //     let shiftedText = replacedText.shift();
-    //     const totalPrice = parseFloat(replacedText[0]) + parseFloat(replacedText[1]);
-    //     const tax = (0.08*totalPrice).toFixed(2);
-    //     const realTotal = (parseFloat(totalPrice) + parseFloat(tax)).toFixed(2);
-
-    //     cy.log(realTotal);
-
-    //     return realTotal;
-    // });
-
-
 };
